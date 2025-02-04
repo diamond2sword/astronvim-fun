7369d6004f7d131f0aa317b0d8011fc1414948d7
diamond2sword <diamond2sword@gmail.com>
2025-02-04 19:15:34 +0800

    update project

--- install_neovim.bash
+++ install_neovim.bash
@@ -1,8 +1,21 @@
 #!/bin/bash
 
-pkg install wget git clang which termux-api lua54
+pkg install wget git clang
 
 {
+	# misc
+	pkg install which termux-api lua54
+}
+
+{
+	# git.bash
+	pkg install gh expect
+}
+
+{
+	# treesitter
+	pkg install clang
+
 	# lsp
 	pkg install lua-language-server clang
 
@@ -13,26 +26,21 @@ pkg install wget git clang which termux-api lua54
 #https://docs.astronvim.com/
 {
 	#https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/JetBrainsMono.zip
-	mkdir -p ~/downloads/JetBrainsMono
-	(
-		cd ~/downloads/JetBrainsMono
-		curl -LJO "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/JetBrainsMono.zip"
-		unzip JetBrainsMono.zip
-		cp JetBrainsMonoNerdFontMono-Regular.ttf ~/.termux/font.ttf
-		termux-reload-settings
-	)
-	rm -rf ~/downloads/JetBrainsMono
+	# mkdir -p ~/downloads/JetBrainsMono
+	# (
+	# 	cd ~/downloads/JetBrainsMono
+	# 	curl -LJO "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/JetBrainsMono.zip"
+	# 	unzip JetBrainsMono.zip
+	# 	cp JetBrainsMonoNerdFontMono-Regular.ttf ~/.termux/font.ttf
+	# 	termux-reload-settings
+	# )
+	# rm -rf ~/downloads/JetBrainsMono
+	mkdir -p ~/.termux
+	curl -LJ -o "~/.termux/font.ttf" "https://raw.githubusercontent.com/diamond2sword/astronvim-fun/refs/heads/main/font/JetBrainsMonoNerdFontMono-Regular.ttf"
 
 	pkg install neovim
-
-	pkg install nodejs-lts
 
 	git clone https://github.com/diamond2sword/astronvim-fun ~/.config/nvim
 
 	nvim
-}
-
-{
-	# git.bash
-	pkg install gh expect
 }
