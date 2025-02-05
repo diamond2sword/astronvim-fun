474c555a574ead72e82159c9e07f823df2f0bc8c
diamond2sword <diamond2sword@gmail.com>
2025-02-06 01:28:02 +0800

    Updated

--- lua/plugins/user/fn/git/push.lua
+++ lua/plugins/user/fn/git/push.lua
@@ -1,24 +1,27 @@
-local push = function(commit_name)
+local get_git_bash_path = function()
   local buf_path = vim.api.nvim_buf_get_name(0)
   local buf_dir = vim.fn.fnamemodify(buf_path, ":h")
   local repo_dir = vim.fn.systemlist("git -C "..buf_dir.." rev-parse --show-toplevel")[1]
   local git_bash_path = repo_dir.."/git.bash"
   if not vim.loop.fs_stat(git_bash_path) then
     vim.notify(" git.bash does not exist in repo", vim.log.levels.WARN)
-    return
+    return nil
   end
-  local current_win = vim.api.nvim_get_current_win()
-  
+  return git_bash_path
+end
+
+local push = function(commit_name)
+  local git_bash_path = get_git_bash_path()
+  if git_bash_path == nil then return end
 
   vim.notify(' Git Push: Pushing...', vim.log.levels.INFO)
-
+  local current_win = vim.api.nvim_get_current_win()
   local cmd = 'bash '..git_bash_path..' push '..commit_name
 
   local Terminal = require("toggleterm.terminal").Terminal
   local term = Terminal:new({
     cmd = cmd,
     hidden = false,
-    display_name = " Git Push: '"..commit_name.."'",
     direction = "horizontal",
     on_exit = function(_, _, exit_code)
       if exit_code == 0 then
