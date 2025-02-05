46f45b44222d2ebdc52ff38bce0d6821fc835108
diamond2sword <diamond2sword@gmail.com>
2025-02-05 23:15:36 +0800

    update project

--- lua/plugins/user/core.lua
+++ lua/plugins/user/core.lua
@@ -33,7 +33,7 @@ return {
 
             local Terminal = require("toggleterm.terminal").Terminal
             local term = Terminal:new({
-              cmd = cmd,
+              cmd = cmd..'\nsleep 3',
               hidden = false,
               direction = "horizontal",
               on_open = function()
