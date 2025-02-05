144cf176a0f6bd33b4a2c79b951ceadfa0373a19
diamond2sword <diamond2sword@gmail.com>
2025-02-05 23:57:32 +0800

    update project

--- lua/plugins/user/core.lua
+++ lua/plugins/user/core.lua
@@ -17,25 +17,11 @@ return {
         ["<Leader>g<C-s>"] = {
           require('plugins.user.fn.git.push'),
           desc = " Git Push"
-        }
+        },
       },
       v = {
         ["<C-c>"] = {
           require('plugins.user.fn.termux.copy'),
-          -- function()
-          --   vim.cmd('normal! "ay')
-          --   local text = vim.fn.getreg('a')
-          --   text = text:gsub("\n*$", "")
-          --   text = text:gsub("^\n*", "")
-          --
-          --   vim.notify(" Copying...", vim.log.levels.INFO)
-          --
-          --   vim.fn.jobstart('cat << "EOF" | termux-clipboard-set\n'..text..'\nEOF', {
-          --     on_exit = function()
-          --       vim.notify(" Copied!", vim.log.levels.INFO)
-          --     end,
-          --   })
-          -- end,
           desc = " Copy",
         },
       },
