862e1896fa1ec46f6c32560eeb1ce4b79d858ac6
diamond2sword <diamond2sword@gmail.com>
2025-02-06 23:00:56 +0800

    Update project

--- lua/plugins/user/fn/toggleterm/cmd.lua
+++ lua/plugins/user/fn/toggleterm/cmd.lua
@@ -14,6 +14,18 @@ local notify_exit_code = function(msg_cmd, msg_ok, msg_err)
   end
 end
 
+local cmds_join = function(cmds)
+  local cmd_result
+  for i, cmd in ipairs(cmds) do
+    if i == 1 then
+      cmd_result = cmd
+    else
+      cmd_result = cmd_result..'\n'..cmd
+    end
+  end
+  return cmd_result
+end
+
 local toggleterm_cmd = function(cmd, opts)
   local Terminal = require("toggleterm.terminal").Terminal
   local current_win = vim.api.nvim_get_current_win()
@@ -32,7 +44,9 @@ local toggleterm_cmd = function(cmd, opts)
     end,
   }
   table_override(default_opts, opts)
-  default_opts.cmd = default_opts.cmd..'\nsleep '..default_opts.sleep
+  local echo_cmd = 'cat << "ECHO_CMD_EOF"\n'..cmd..'\nECHO_CMD_EOF'
+  local sleep_cmd = 'sleep '..default_opts.sleep
+  default_opts.cmd = cmds_join({echo_cmd, default_opts.cmd, sleep_cmd})
 
   local term = Terminal:new(default_opts)
   term:toggle()
